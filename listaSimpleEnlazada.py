3dd4e0eaa1f2e86c80ada1d201d2df8a8ca9a31e
Jaime Armira <tec-james.armira@outlook.com>
2021-03-19 23:49:11 -0600

    update

---
+++ listaSimpleEnlazada.py
@@ -0,0 +1,95 @@
+class Nodo:
+    def __init__ (self,fila,columna,dato):
+        self.CoorX=fila
+        self.CoorY=columna
+        self.Dato=dato
+        self.Siguiente=None
+
+
+class listaEnlazadaMatriz:
+    def __init__(self):
+        self.inicio=None
+        self.cola=None
+        self.tamaño=0
+        
+    def add(self,fila,columna,dato):
+        nodo=Nodo(fila,columna,dato)
+        self.tamaño +=1
+        
+        if self.inicio:
+            self.inicio.Siguiente=nodo
+            self.inicio=nodo
+        else:
+            self.inicio=nodo
+            self.cola=nodo
+            
+    def iterar(self):
+        actual = self.cola
+
+        while actual:
+            dato = actual.Dato
+            x=actual.CoorX
+            y=actual.CoorY
+            actual = actual.Siguiente
+            yield x,y,dato
+    
+    def crearlista(self,b,v):
+        dato=""
+        for x in range(b):
+            for y in range(v):
+               for n in self.iterar():
+                    if x+1==int(n[0]) and y+1==int(n[1]): 
+                        if y+1==1:
+                            dato+="["+str(n[2])+","
+                        elif y+1==v and (x+1)!=b:
+                            dato+=str(n[2])+"]/"
+                        elif x+1==b and y+1==v:
+                            dato+=str(n[2])+"]"
+                        else:
+                            dato+=str(n[2])+","
+        dato=dato.split("/")
+        return dato  
+                      
+    def buscar(self,x,y):
+        for n in self.iterar():
+            if x==int(n[0]) and y==int(n[1]):
+                return n[2]
+   
+    def eliminar(self,x,y):
+        actual=self.cola
+        anterior=self.cola
+        
+        while actual:
+            if int(actual.CoorX)==x and int(actual.CoorY)==y:
+                if actual==self.cola:
+                    self.cola=actual.Siguiente
+                else:
+                    # suponermos que tengo [1]->[2]->[3]
+                    #ahora quiero eliminar [2]
+                    #mi resultado [1]->[3]
+                    anterior.Siguiente=actual.Siguiente
+                self.tamaño-=1
+                return
+            anterior=actual
+            actual=actual.Siguiente
+#    def deletCola(self):
+#        actual=self.cola
+        
+#        if not actual:
+#           anterior=actual.Siguiente
+#           actual=actual.Siguiente
+#           self.tamaño-=1
+#           return True
+#        else:
+#            return False
+#        return False        
+#def prueba(x,y,dato):
+   # lista=listaEnlazadaMatriz()
+   # lista.add(0,0,1)
+   # lista.add(0,1,5)
+
+
+#def imprimir():
+    #lista=listaEnlazadaMatriz()
+   # for d in lista.iterar():
+    #    print(d[2])
--- matrizOctagonal.py
+++ matrizOctagonal.py
@@ -1,13 +1,14 @@
 class NodoMatrizOcta:
-    def __init__(self,x,y,dato,arriba,abajo,izquierda,derecha):
+    def __init__(self,x,y,dato):
         self.x=x
         self.y=y
         self.dato=dato
-        self.arriba=arriba
-        self.abajo=abajo
-        self.izquierda=izquierda
+        self.arriba=None
+        self.abajo=None
+        self.izquierda=None
+        self.derecha=None
 class NodoCabecera:
-    def __init__(self,tipo,indice,siguiente,derecha,abajo):
+    def __init__(self,tipo=None,indice=None,siguiente=None,derecha=None,abajo=None):
         self.tipo=tipo
         self.indice=indice
         self.siguiente=siguiente
@@ -84,4 +85,106 @@ class matriz_Ortogonal:
             self.insert_cabecera(NodoAuxiliar.NodoColumnas,x,"Columna")
             self.insertar_nodoFila(nodo)
             self.insert_nodoColumna(nodo)
-            
+    def buscar(self,x,y):
+        nodo=self.NodoRaiz.NodoColumnas
+        while nodo is not None:
+            NodoAuxiliar=nodo.abajo
+            while NodoAuxiliar is not None:
+                if NodoAuxiliar.x==x and NodoAuxiliar.y==y:
+                    return True
+                NodoAuxiliar=NodoAuxiliar.abajo
+            nodo=nodo.siguiente
+        return False
+    
+    
+import os
+
+
+nueva_matriz = matriz_Ortogonal()
+
+nueva_matriz.insertar(2,5,"nuevo nodo")
+nueva_matriz.insertar(2,3,"nuevo nodo")
+nueva_matriz.insertar(2,4,"nuevo nodo")
+nueva_matriz.insertar(2,9,"nuevo nodo")
+nueva_matriz.insertar(3,7,"nuevo nodo")
+nueva_matriz.insertar(3,1,"nuevo nodo")
+nueva_matriz.insertar(3,8,"nuevo nodo")
+nueva_matriz.insertar(3,9,"nuevo nodo")
+nueva_matriz.insertar(6,6,"nuevo nodo")
+nueva_matriz.insertar(6,1,"nuevo nodo")
+nueva_matriz.insertar(6,5,"nuevo nodo")
+nueva_matriz.insertar(6,9,"nuevo nodo")
+nueva_matriz.insertar(8,3,"nuevo nodo")
+nueva_matriz.insertar(8,4,"nuevo nodo")
+nueva_matriz.insertar(9,8,"nuevo nodo")
+nueva_matriz.insertar(9,9,"nuevo nodo")
+nueva_matriz.insertar(8,1,"nuevo nodo")
+nueva_matriz.insertar(10,10,"nuevo nodo")
+nueva_matriz.insertar(10,1,"nuevo nodo")
+nodo = nueva_matriz.NodoRaiz.NodoColumnas
+
+'''
+nodo = nueva_matriz.NodoRaiz.NodoFilas
+while(nodo is not None):
+    nodo_temp = nodo.derecha
+    while(nodo_temp is not None):
+        print(str(nodo_temp.x)+str(nodo_temp.y))
+        nodo_temp=nodo_temp.derecha
+    nodo=nodo.siguiente
+    
+print("FIN")
+nodo = nueva_matriz.NodoRaiz.NodoColumnas
+while(nodo is not None):
+    nodo_temp = nodo.abajo
+    while(nodo_temp is not None):
+        print(str(nodo_temp.x)+str(nodo_temp.y))
+        nodo_temp=nodo_temp.abajo
+    nodo=nodo.siguiente
+    
+print("FIN")
+'''
+
+def graficar_matriz():
+    grafo = "digraph"
+    grafo+=str("{\nnode[shape=record];\n")
+    grafo+=str("graph[pencolor=transparent];\n")
+    #grafo+=str("rankdir=LR;\n")
+    grafo+=str("node [style=filled];\n")
+    nodo = nueva_matriz.NodoRaiz.NodoFilas
+
+    for y in range(1, 11):
+        nodo_temp = nodo.derecha
+        for x in range(1, 11):
+            if(nueva_matriz.buscar(x,y)):
+                grafo+=str("p"+str(x)+str(y)+"[label=\"{<data>"+str(x)+","+str(y)+"|<next>}\" pos=\""+str(x)+","+str(10-y)+"!\"];\n")
+                if(nodo_temp.derecha!=None): 
+                    nodo_2=nodo_temp
+                    nodo_temp=nodo_temp.derecha
+                    grafo+=str("p"+str(nodo_2.x)+str(nodo_2.y)+"->"+"p"+str(nodo_temp.x)+str(nodo_temp.y)+"[dir=both];\n")
+            else:
+                pass
+            if nodo.siguiente!=None:
+                if nodo.siguiente.indice==y+1:
+                    nodo=nodo.siguiente    
+    nodo = nueva_matriz.NodoRaiz.NodoColumnas
+    for x in range(1, 11):
+        nodo_temp = nodo.abajo
+        for y in range(1, 11):
+            if(nueva_matriz.buscar(x,y)):
+                if(nodo_temp.abajo!=None):
+                    nodo_2=nodo_temp
+                    nodo_temp=nodo_temp.abajo
+                    grafo+=str("p"+str(nodo_2.x)+str(nodo_2.y)+"->"+"p"+str(nodo_temp.x)+str(nodo_temp.y)+"[dir=both];\n")
+            else:
+                pass
+            if nodo.siguiente!=None:
+                if nodo.siguiente.indice==x+1:
+                    nodo=nodo.siguiente         
+    grafo+=str("}\n")
+    f= open("ejemplo.dot","w+")
+    f.write(grafo)
+    f.close()   
+    os.system("fdp -Tpng -o graph-g.png ejemplo.dot")
+
+
+graficar_matriz()
